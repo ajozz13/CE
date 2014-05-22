b767b88b2a667d8192a50066a0e76f505000f344
Alberto Ochoa <ajozz13@gmail.com>
2014-05-21 21:47:03 -0400

    updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519023143) do
+ActiveRecord::Schema.define(version: 20140522012846) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -62,9 +62,9 @@ ActiveRecord::Schema.define(version: 20140519023143) do
   create_table "trackings", force: true do |t|
     t.string   "package_id"
     t.datetime "times"
-    t.integer  "action_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "trackcode_id"
   end
 
 end
